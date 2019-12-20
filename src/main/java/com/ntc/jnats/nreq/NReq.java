9ccfb961738c4faf868deb0f617a78f4255c0a61
nghiatc <congnghia0609@gmail.com>
2019-12-20 19:59:43 +0700

    update code

--- src/main/java/com/ntc/jnats/nreq/NReq.java
+++ src/main/java/com/ntc/jnats/nreq/NReq.java
@@ -16,11 +16,133 @@
 
 package com.ntc.jnats.nreq;
 
+import com.ntc.configer.NConfig;
+import com.ntc.jnats.NConnection;
+import io.nats.client.Connection;
+import io.nats.client.Message;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author nghiatc
  * @since Dec 19, 2019
  */
 public class NReq {
+    private static final Logger log = LoggerFactory.getLogger(NReq.class);
+    private static Map<String, NReq> mapNRequest = new ConcurrentHashMap<>();
+    private static Lock lock = new ReentrantLock();
+    private NConnection nConn;
+    private Duration timeout = Duration.ofSeconds(60); // Default 60s.
 
+    private NReq() {
+    }
+    
+    public NReq(String name) throws IOException, InterruptedException {
+        long timeout = NConfig.getConfig().getLong(name+".timeout", 0L);
+        if (timeout > 0) {
+            this.timeout = Duration.ofSeconds(timeout);
+        }
+        this.nConn = new NConnection(name);
+    }
+    
+    public static NReq getInstance(String name) {
+        if(name == null || name.isEmpty()) {
+            return null;
+        }
+        NReq instance = mapNRequest.containsKey(name) ? mapNRequest.get(name) : null;
+		if(instance == null) {
+			lock.lock();
+			try {
+                instance = mapNRequest.containsKey(name) ? mapNRequest.get(name) : null;
+				if(instance == null) {
+					instance = new NReq(name);
+                    mapNRequest.put(name, instance);
+				} else {
+                    if (!instance.isOpen()) {
+                        instance.close();
+                        instance = new NReq(name);
+                        mapNRequest.put(name, instance);
+                    }
+                }
+			} catch (Exception e) {
+                log.error(e.getMessage(), e);
+            } finally {
+				lock.unlock();
+			}
+		} else {
+            if (!instance.isOpen()) {
+                lock.lock();
+                try {
+                    instance.close();
+                    instance = new NReq(name);
+                    mapNRequest.put(name, instance);
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+		return instance;
+	}
+
+    public NConnection getNConn() {
+        return nConn;
+    }
+    
+    public boolean isOpen() {
+        return this.nConn.getConnection().getStatus() == Connection.Status.CONNECTED;
+    }
+    
+    public void close() throws InterruptedException {
+        this.nConn.close();
+    }
+    
+    public Message publish(String subject, String msg) {
+        Message ret = null;
+        try {
+            if (!subject.isEmpty() && !msg.isEmpty()) {
+                ret = this.nConn.getConnection().request(subject, msg.getBytes(StandardCharsets.UTF_8), this.timeout);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            return ret;
+        }
+    }
+    
+    public Message publish(String subject, String msg, Duration timeout) {
+        Message ret = null;
+        try {
+            if (!subject.isEmpty() && !msg.isEmpty()) {
+                ret = this.nConn.getConnection().request(subject, msg.getBytes(StandardCharsets.UTF_8), timeout);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            return ret;
+        }
+    }
+    
+    public CompletableFuture<Message> publishAsyn(String subject, String msg) {
+        CompletableFuture<Message> ret = null;
+        try {
+            if (!subject.isEmpty() && !msg.isEmpty()) {
+                ret = this.nConn.getConnection().request(subject, msg.getBytes(StandardCharsets.UTF_8));
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            return ret;
+        }
+    }
 }
